dd449785d4b2bc592cc460bf8ccc99544a7fa080
Bally Lomibao <Bally.Lomibao@howick.co.nz>
2022-12-02 16:42:10 +1300

    almost done

--- main.js
+++ main.js
@@ -2,13 +2,25 @@ const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const url = require('url')
 
-let win
+process.env.NODE_ENV !== 'production'
 
+const isDev = process.env.NODE_ENV !== "production";
+const isWin = process.platform === "win32";
+
+let win
 
 function createWin() {
 
     //create a new window
-    win = new BrowserWindow({width: 800, height: 600, icon:__dirname+'/img/sysinfo.png'})
+    win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      icon: __dirname + "/img/sysinfo.png",
+      webPreferences: {
+        contextIsolation: false,
+        nodeIntegration: true,
+      },
+    });
 
     //load html file
     win.loadURL(url.format({
@@ -19,12 +31,11 @@ function createWin() {
 
 
     //open devtools
-    win.webContents().openDevTools()
+    win.webContents.openDevTools()
 
     win.on('closed', () => {
         win = null
-    })
-}
+    })}
 
 
 app.on('ready', createWin)
@@ -33,5 +44,5 @@ app.on('ready', createWin)
 //quit when window is closed
 app.on('window-all-closed', () => {
     if(process.platform !== 'darwin') {
-
+        app.quit()
     }})
